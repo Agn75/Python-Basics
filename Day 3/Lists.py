aa783352deddc77f45a60f9032c947c9b389f5a6
Agn75 <Alfredogn75@gmail.com>
2023-07-10 18:49:14 -0600

    Lists and String's properties added

---
+++ Day 3/Lists.py
@@ -0,0 +1,14 @@
+my_list = ["a", "b", "c"]
+my_list2 = ["d", "e", "f"]
+my_list3 = my_list + my_list2
+
+# Append Method adds a new element to the list
+my_list3.append('g')
+
+# Pop method removes the last element on the list unless you add the index as a parameter
+my_list3.pop()
+
+# Sort method rearranges the order of the object of the lists
+my_list3.sort()
+
+print(my_list3)
---
+++ Day 3/String-Properties.py
@@ -0,0 +1,10 @@
+# We can have String paragraphs by using """ x """
+poem = """Horizon to horizon   the guns
+draw on what we’re too easily known
+the poor drawing on their own"""
+
+# "Len" property allow us to count the number of characters in the string
+print(len(poem))
+
+# find an specific word/character in the sting, returning true and false values
+print("easily" in poem)
